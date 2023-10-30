e323d8b76c85ed4ca7b32ba8ed856d2ee1612e66
Kobe Vervoort <vervoortkobe@outlook.com>
2023-10-30 14:25:32 +0100

    Added KeyboardReader

---
+++ gamedevproject/InputClasses/KeyboardReader.cs
@@ -0,0 +1,24 @@
+﻿using gamedevproject.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamedevproject.InputClasses
+{
+    class KeyboardReader : IInputReader
+    {
+        public Vector2 ReadInput()
+        {
+            KeyboardState state = Keyboard.GetState();
+            Vector2 direction = Vector2.Zero;
+            if(state.IsKeyDown(Keys.Left)) direction.X -= 1;
+            if(state.IsKeyDown(Keys.Right)) direction.X += 1;
+            return direction;
+        }
+    }
+
+}
---
+++ gamedevproject/Interfaces/IInputReader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamedevproject.Interfaces
+{
+    interface IInputReader
+    {
+        Vector2 ReadInput();
+    }
+}
