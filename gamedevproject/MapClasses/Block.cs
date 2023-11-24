75f5eb1233ccbbbbbd87ee95398254aa4c8b30d4
Kobe Vervoort <vervoortkobe@outlook.com>
2023-11-24 16:09:51 +0100

    Blocks concept

---
+++ gamedevproject/MapClasses/Block.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamedevproject.MapClasses
+{
+    public class Block
+    {
+        private int tileId { get; set; }
+        private Vector2 position { get; set; }
+
+        public Block(int tileId, Vector2 position)
+        {
+            this.tileId = tileId;
+            this.position = position;
+        }
+        public void Draw(SpriteBatch spriteBatch, Texture2D t)
+        {
+
+        }
+    }
+}
---
+++ gamedevproject/MapClasses/Ground.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamedevproject.MapClasses
+{
+    public class Ground : Block
+    {
+        Ground(Rectangle size) : base(1, new Vector2(1, 1))
+        {
+
+        }
+    }
+}
